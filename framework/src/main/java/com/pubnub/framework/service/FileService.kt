6aa972d84e3fb345b4b69216d3a337064ee96f9a
Marcin Sroda <marcin.sroda@pubnub.com>
2021-04-19 18:23:05 +0200

    SDK-842
    Cipher key removed (use init one)

--- framework/src/main/java/com/pubnub/framework/service/FileService.kt
+++ framework/src/main/java/com/pubnub/framework/service/FileService.kt
@@ -28,7 +28,6 @@ class FileService {
             message = message,
             shouldStore = false,
             meta = meta,
-            cipherKey = pubNub.configuration.cipherKey,
         ).coroutine()
 
     suspend fun list(channel: ChannelId, limit: Int? = null, next: PNPage.PNNext? = null) =
